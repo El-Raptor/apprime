dda7d4370ab71210f1b47cb23676521db63d5803
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-11-26 14:06:58 -0400

    mend

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,9 @@ import { Button } from '@material-ui/core';
 export const ButtonStyled = styled(Button)`
     background: linear-gradient(to right, #E36C09, #F09949);
     color: white;
-
+    box-shadow: 1px 3px 4px 0px rgba(0,0,0,0.2);
     border-radius: ${({ round }) => (round ? '100px' : '0px')};
-    font-size: ${({ large }) => (large ? '2rem' : '1.2rem')};
+    font-size: ${({ large }) => (large ? '2rem' : '1.2rem')};  
     padding: ${({ large }) => (large ? '.5rem 4rem' : '.2rem 3.2rem')};
     transition: 0.3s;
 
